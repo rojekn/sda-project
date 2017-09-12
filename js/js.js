be935368be0590e72abba7672f5cd0183b2411ed
rojek <rojek_natalia@wp.pl>
2017-09-12 03:03:14 +0200

    fix navbar-hamburger

--- js/js.js
+++ js/js.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
     $(document).scroll(function(){
         var $navbar = $('.navbar');
         if($(document.body).scrollTop() > 0) {
-            $navbar.css({'background-color': '#555', 'transform': 'scale(1, 0.8)', 'top': '-10px'});
+            $navbar.css({'background-color': '#555', 'opacity': '0.8', 'transform': 'scale(1, 0.8)', 'top': '-10px'});
         } else {
-             $navbar.css({'background-color': 'transparent', 'transform': null, 'top': '0px'});
+             $navbar.css({'background-color': 'transparent', 'transform': 'scale(1, 1)', 'top': '0px'});
         }
     })
 })
