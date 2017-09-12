e4b65dbf1ded59ca1f159afd83f2a17717b2a0db
rojek <rojek_natalia@wp.pl>
2017-09-12 15:29:12 +0200

    add contact form, php files

---
+++ form/send.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once 'class.phpmailer.php';
+
+$_POST = array_map('trim', $_POST);
+
+$error = false;
+
+if (!isset($_POST['contact_name']) || empty($_POST['contact_name'])) {
+    $error = true;
+}
+
+if (!isset($_POST['contact_email']) || empty($_POST['contact_email']) || !PHPMailer::ValidateAddress($_POST['contact_email'])) {
+    $error = true;
+}
+
+if (!isset($_POST['contact_body']) || empty($_POST['contact_body'])) {
+    $error = true;
+}
+
+if ($error) {
+    exit('nok');
+}
+
+$mail = new PHPMailer();
+
+$mail->CharSet = 'UTF-8';
+
+$mail->SetFrom($_POST['contact_email'], $_POST['contact_name']);
+$mail->AddAddress('rojek_natalia@wp.pl');
+$mail->Subject = 'Wiadomość ze strony internetowej';
+$mail->Body = $_POST['contact_body'];
+
+if (!$mail->Send()) {
+    exit('nok');
+}
+
+exit('ok');
--- js/js.js
+++ js/js.js
@@ -1,11 +1,62 @@
-$(document).ready(function(){
-    
-    $(document).scroll(function(){
+$(document).ready(function () {
+
+    $(document).scroll(function () {
         var $navbar = $('.navbar');
-        if($(document.body).scrollTop() > 0) {
-            $navbar.css({'background-color': '#555', 'opacity': '0.8', 'transform': 'scale(1, 0.8)', 'top': '-10px'});
+        if ($(document.body).scrollTop() > 0) {
+            $navbar.css({ 'background-color': '#555', 'opacity': '0.8', 'transform': 'scale(1, 0.8)', 'top': '-10px' });
         } else {
-             $navbar.css({'background-color': 'transparent', 'transform': 'scale(1, 1)', 'top': '0px'});
+            $navbar.css({ 'background-color': 'transparent', 'transform': 'scale(1, 1)', 'top': '0px' });
         }
-    })
+    });
+
+    $('a[href^="#"]').on('click', function (event) {
+
+        var target = $($(this).attr('href'));
+
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+    $(function () {
+
+        var sending = false;
+        $('#contactform').on('submit', function (e) {
+            var $form = $(this), $submit = $('button[type="submit"]', $form);
+
+            e.preventDefault();
+
+            if (sending) {
+                return false;
+            }
+
+            $('.form-error', $form).remove();
+
+            $('input, textarea', $form).prop('readonly', true);
+            $submit.val('Wysyłam…');
+            sending = true;
+
+            $.post($form.attr('action'), $form.serialize(), function (data) {
+                if (data === 'ok') {
+                    $form.slideUp('fast', function () {
+                        $form.after('<div class="form-success">Wiadomość została wysłana! Skontaktujemy się z Tobą jak tylko to będzie możliwe.</div>');
+                    });
+
+                    return true;
+                }
+
+                $form.prepend('<div class="form-error">Wystąpił błąd podczas wysyłania formularza. Upewnij się, że wypełniłeś wszystkie pola i poprawnie rozwiązałeś działanie.</div>');
+                $('input, textarea', $form).prop('readonly', false);
+                $submit.val('Wyślij wiadomość');
+                sending = false;
+
+                return false;
+            }, 'text');
+        });
+    });
+
+
+
 })
